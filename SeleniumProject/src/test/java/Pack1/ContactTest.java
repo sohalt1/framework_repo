51e177cb3a4e625ae51bb53c8ddb8a0bddd8adfe
sohal <sohal@Dell>
2025-02-21 16:27:23 +0530

    Push

---
+++ SeleniumProject/src/test/java/Pack1/ContactTest.java
@@ -0,0 +1,29 @@
+package Pack1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test 
+	public void createContactTest()
+	{
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("execute create contact Test");
+	}
+	@Test
+	
+	public void modifyContacttest()
+	{
+		System.out.println("execute modify contact Test");
+	}
+}
+
+
+
---
+++ SeleniumProject/src/test/java/Pack2/OrgTest.java
@@ -0,0 +1,20 @@
+package Pack2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test 
+	public void createOrgTest()
+	{
+		System.out.println("execute create Org Test");
+	}
+	@Test
+	
+	public void modifyOrgtest()
+	{
+		System.out.println("execute modify Org Test");
+	}
+}
+
+
